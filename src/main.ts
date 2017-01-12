fd5e8b4228ade127deca82bcd9d890709a549f0d
Gabriel Ferreiro <gbril9119@gmail.com>
2017-01-12 22:50:12 +0100

    Pinto un primer cuadrado :)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('hi');
+function hello() {
+    const mainCanvas: HTMLCanvasElement = document.getElementById('main-canvas') as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = mainCanvas.getContext('2d');
+    const size = {
+        width: mainCanvas.width,
+        height: mainCanvas.height
+    };
+
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, size.width, size.height);
+
+    ctx.fillStyle = 'rgb(255,255,255)';
+    ctx.fillRect(size.width / 2 - 25, size.height / 2 - 25, 50, 50);
+}
+
+window.requestAnimationFrame(hello);
